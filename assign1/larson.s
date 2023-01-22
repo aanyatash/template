197999a5c8d905a0c5895bd333ab8b8972516a4a
Aanya Tashfeen <aanyatash@gmail.com>
2023-01-22 15:56:48 -0800

    Working Larson Scanner

--- assign1/larson.s
+++ assign1/larson.s
@@ -8,8 +8,8 @@
  * Make sure to use GPIO pins 20-23 (or 20-27) for your scanner.
  */
 
-.equ DELAY, 0x3F0000
-.equ PINS, 0b0111  // number of pins we're using is 8
+.equ DELAY, 0x7E000
+.equ PINS, 0x8  // number of pins we're using is 8
 
 mov r2, #PINS // store number of pins in r2
 // configure GPIO20 for output
@@ -31,9 +31,7 @@ looptest:
 //mov r1, #(1<<20)
 //mov r3, #0x8
 
-
 testright:
-    subs r3, #1
     ldr r0, SET0
     str r1, [r0] 
 
@@ -47,14 +45,13 @@ testright:
     ldr r0, CLR0
     str r1, [r0]
     mov r1, r1, LSL #1
-
+    subs r3, #1
 bne testright
 
-//mov r1, #(1<<20)
+mov r1, #(1<<26)
 mov r4, #0x8
 
 testleft:
-    subs r4, #1
     ldr r0, SET0
     str r1, [r0] 
 
@@ -68,113 +65,115 @@ testleft:
     ldr r0, CLR0
     str r1, [r0]
     mov r1, r1, LSR #1
+    subs r4, #1
 
 bne testleft
 
-//mov r3, #0x8
+mov r3, #0x8
+mov r1, #(1<<21)
 
 b looptest
 
-mov r3, #PINS
-sub r3, r3, #1
-// set bit 20
-mov r1, #(1<<20)
-
-    // set GPIO 20 high
-    ldr r0, SET0
-    str r1, [r0] 
-
-    // delay
-    mov r2, #DELAY
-    wait_initial:
-        subs r2, #1
-        bne wait_initial
-
-    // set GPIO 20 low
-    ldr r0, CLR0
-    str r1, [r0]
-
-    // delay
-    mov r2, #DELAY
-    wait_initial2:
-        subs r2, #1
-        bne wait_initial2
-
-
-loop: 
-
-scanner_right:
-
-    subs r3, #1
-    mov r1, r1, LSL #1
-
-    // set GPIO 20 high
-    ldr r0, SET0
-    str r1, [r0] 
-
-    // delay
-    mov r2, #DELAY
-    wait1:
-        subs r2, #1
-        bne wait1
-
-    // set GPIO 20 low
-    ldr r0, CLR0
-    str r1, [r0]
-
-    //mov r1, r1, LSL #1
-    //mov r1, #(1<<21)
- 
-    // delay
-    mov r2, #DELAY
-    wait2:
-        subs r2, #1
-        bne wait2
-
-   
-    bne scanner_right
-
-mov r3, #PINS
-sub r3, r3, #1
-//mov r1, r1, LSR #1
-
-scanner_left:
-
-    subs r3, #1
-    mov r1, r1, LSR #1
-    
-    // set GPIO 20 high
-    ldr r0, SET0
-    str r1, [r0] 
-
-    // delay
-    mov r2, #DELAY
-    wait1_left:
-        subs r2, #1
-        bne wait1_left
-
-    // set GPIO 20 low
-    ldr r0, CLR0
-    str r1, [r0]
-
-    // delay
-    mov r2, #DELAY
-    wait2_left:
-        subs r2, #1
-        bne wait2_left
-
-
-  //  mov r1, r1, LSR #1
-    //mov r1, #(1<<21)
-    
-   bne scanner_left
-
-mov r3, #PINS
-sub r3, r3, #1
-// set bit 20
-//mov r1, #(1<<20)
-
-b loop
+#mov r3, #PINS
+#sub r3, r3, #1
+#// set bit 20
+#mov r1, #(1<<20)
+#
+#    // set GPIO 20 high
+#    ldr r0, SET0
+#    str r1, [r0] 
+#
+#    // delay
+#    mov r2, #DELAY
+#    wait_initial:
+#        subs r2, #1
+#        bne wait_initial
+#
+#    // set GPIO 20 low
+#    ldr r0, CLR0
+#    str r1, [r0]
+#
+#    // delay
+#    mov r2, #DELAY
+#    wait_initial2:
+#        subs r2, #1
+#        bne wait_initial2
+#
+#
+#loop: 
+#
+#scanner_right:
+#
+#    subs r3, #1
+#    mov r1, r1, LSL #1
+#
+#    // set GPIO 20 high
+#    ldr r0, SET0
+#    str r1, [r0] 
+#
+#    // delay
+#    mov r2, #DELAY
+#    wait1:
+#        subs r2, #1
+#        bne wait1
+#
+#    // set GPIO 20 low
+#    ldr r0, CLR0
+#    str r1, [r0]
+#
+#    //mov r1, r1, LSL #1
+#    //mov r1, #(1<<21)
+# 
+#    // delay
+#    mov r2, #DELAY
+#    wait2:
+#        subs r2, #1
+#        bne wait2
+#
+#   
+#    bne scanner_right
+#
+#mov r3, #PINS
+#sub r3, r3, #1
+#//mov r1, r1, LSR #1
+#
+#scanner_left:
+#
+#    subs r3, #1
+#    mov r1, r1, LSR #1
+#    
+#    // set GPIO 20 high
+#    ldr r0, SET0
+#    str r1, [r0] 
+#
+#    // delay
+#    mov r2, #DELAY
+#    wait1_left:
+#        subs r2, #1
+#        bne wait1_left
+#
+#    // set GPIO 20 low
+#    ldr r0, CLR0
+#    str r1, [r0]
+#
+#    // delay
+#    mov r2, #DELAY
+#    wait2_left:
+#        subs r2, #1
+#        bne wait2_left
+#
+#
+#  //  mov r1, r1, LSR #1
+#    //mov r1, #(1<<21)
+#    
+#   bne scanner_left
+#
+#mov r3, #PINS
+#sub r3, r3, #1
+#// set bit 20
+#//mov r1, #(1<<20)
+#
+#b loop
  
  FSEL0: .word 0x20200000
  FSEL1: .word 0x20200004
