d7cfc98b1849907bb7438d19f35e181b178da658
Aanya Tashfeen <aanyatash@gmail.com>
2023-01-23 21:27:01 -0800

    three LED brightnesses with some errors

--- assign1/larson.s
+++ assign1/larson.s
@@ -8,7 +8,7 @@
  * Use GPIO pins 20-27 for scanner
  */
 
-.equ DELAY, 0x8FFFFFFF
+.equ DELAY, 0x3F00
 .equ MID_BRIGHT_ON,0xFC
 .equ MID_BRIGHT_OFF,0x762
 .equ LOW_BRIGHT_ON,0x7E
@@ -27,117 +27,114 @@ pins_output:
 ldr r0, FSEL2 // stores FSEL2 address in r0
 str r1, [r0] // makes GPIO 20-27 an output pin
 
-mov r1, #(1<<20) // sets voltage of leftmost pin GPIO as high 
+#mov r1, #(1<<20) // sets voltage of leftmost pin GPIO as high 
+#mov r3, #PINS
+#//mov r5, #(1<<21)
+#
+#loopbright:
+#
+#lscanner_right:
+#
+#    // set GPIO high
+#    //ldr r0, SET0
+#    //str r1, [r0]
+#
+#    // delay
+#    mov r2, #DELAY
+#    lwait1:
+#
+#       mov r4, #LOW_BRIGHT_ON
+#	ldr r0, SET0
+#	str r1, [r0]
+#	brighton:
+#	    sub r2, r2, #20
+#	    subs r4, #1
+#            bne brighton
+#
+#	mov r4, #LOW_BRIGHT_OFF
+#	ldr r0, CLR0
+#	str r1, [r0]
+#        brightoff:
+#	    sub r2, r2, #20
+#	    subs r4, #1
+#            bne brightoff
+#
+#        subs r2, #20
+#        blt lwait1
+#
+#    // set GPIO low
+#    ldr r0, CLR0
+#    str r1, [r0]
+#    mov r1, r1, LSL #1 // moves on to pin to the right
+#    subs r3, #1 // counter for each pin
+#    bne lscanner_right
+#
+#mov r1, #(1<<20)
+#mov r3, #PINS
+#
+#b loopbright
+
+
+
+
+
+mov r1, #(1<<21) // sets voltage of leftmost pin GPIO as high 
 mov r3, #PINS
-//mov r5, #(1<<21)
+mov r5, #(1<<20) // low bright
+mov r6, #(1<<22) // mid bright
+add r1, r1, r5
+add r1, r1, r6
 
-loopbright:
+loop:
 
-lscanner_right:
-
+// moves through each LED from left to right
+scanner_right:
+    
     // set GPIO high
-    //ldr r0, SET0
-    //str r1, [r0]
-
-    // delay
+  //  ldr r0, SET0
+  //  str r1, [r0]
+   
     mov r2, #DELAY
-    lwait1:
+    wait1:
+        mov r4, #LOW_BRIGHT_ON
+        mov r7, #MID_BRIGHT_ON
+        ldr r0, SET0
+        str r1, [r0]
 
-       mov r4, #LOW_BRIGHT_ON
-	ldr r0, SET0
-	str r1, [r0]
-	brighton:
-	    sub r2, r2, #20
-	    subs r4, #1
-            bne brighton
-
-	mov r4, #LOW_BRIGHT_OFF
+	both_on_wait:
+	    sub r7, r7, #1 // Mid on counter
+//            sub r2, r2, #1 // Total delay counter
+	    subs r4, #1 // Low on counter
+            bne both_on_wait
+       	mov r4, #LOW_BRIGHT_OFF
+	//sub r1, r1, r5
+	ldr r0, CLR0 // SAME REGISTER AS SET
+	str r5, [r0] // turn low off
+	one_on_wait:
+	    sub r4, r4, #1 // Low off counter
+//	    sub r2, r2, #1 // Total delay counter
+	    subs r7, #1 // Mid on counter
+	    bne one_on_wait
+	//sub r1, r1, r6	
 	ldr r0, CLR0
-	str r1, [r0]
-        brightoff:
-	    sub r2, r2, #20
-	    subs r4, #1
-            bne brightoff
-
-        subs r2, #20
-        blt lwait1
+	str r6, [r0] // turn mid off
+        both_off_wait:
+//	    sub r2, r2, #1 // Total delay counter
+	    subs r4, #1 // Low off counter
+            bne both_off_wait
+//	ldr r0, SET0
+ //       str r1, [r0]
+        subs r2, #1
+        bne wait1
 
     // set GPIO low
     ldr r0, CLR0
     str r1, [r0]
-    mov r1, r1, LSL #1 // moves on to pin to the right
+    mov r1, r1, LSL #1 // moves on to pins to the right
     subs r3, #1 // counter for each pin
-    bne lscanner_right
 
-mov r1, #(1<<20)
-mov r3, #PINS
+bne scanner_right
 
-b loopbright
-
-
-
-
-
-#mov r1, #(1<<20) // sets voltage of leftmost pin GPIO as high 
-#mov r3, #PINS
-#mov r5, #(1<<21) // low bright
-#add r1, r1, r5
-#//mov r6, #(1<<19)
-#//add r1, r1, r6
-#
-#loop:
-#
-#// moves through each LED from left to right
-#scanner_right:
-#
-#    // set GPIO high
-#//    ldr r0, SET0
-#//    str r1, [r0]
-#   
-#    mov r2, #DELAY
-#    wait1:
-#        mov r4, #LOW_BRIGHT_ON
-#        mov r7, #MID_BRIGHT_ON
-#	ldr r0, SET0
-#	str r1, [r0]
-#	lowbrighton:
-#	    subs r4, #1
-#//	    sub r7, r7, #1
-#            bne lowbrighton
-#       	mov r4, #LOW_BRIGHT_OFF
-#	mov r7, #MID_BRIGHT_OFF
-#	sub r1, r1, r5
-#	ldr r0, CLR0
-#	str r1, [r0]
-#//	midbrighton:
-#//	    subs r7, #1
-#//	    sub r4, r4, #1
-#//	    bne midbrighton
-#//	sub r1, r1, r6	
-#//	ldr r0, CLR0
-#//	str r1, [r0]
-#        lowbrightoff:
-#	    subs r4, #1
-#            bne lowbrightoff
-#        add r1, r1, r5
-#//	add r1, r1, r6
-#        subs r2, #1
-#        bne wait1
-#
-#    // delay
-#//    mov r2, #DELAY
-# //   wait1:
-#  //      subs r2, #1
-#    //    bne wait1
-#
-#    // set GPIO low
-#   // ldr r0, CLR0
-#   // str r1, [r0]
-#    mov r1, r1, LSL #1 // moves on to pin to the right
-#    subs r3, #1 // counter for each pin
-#
-#bne scanner_right
-#
 #mov r1, r1, LSR #1 // skips over end LED when going back
 #mov r3, #PINS // restart counter for pins
 #
@@ -164,8 +161,8 @@ b loopbright
 #
 #mov r3, #PINS // resets pin counter
 #mov r1, r1, LSL #1 // skips over end LED when going back
-#
-#b loop
+
+b loop
 
 
  FSEL0: .word 0x20200000
