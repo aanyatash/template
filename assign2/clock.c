9aac84f171c5de525d80e48a70d36828d6954f3b
Aanya Tashfeen <aanyatash@gmail.com>
2023-01-30 17:14:46 -0800

    Working clock

--- assign2/clock.c
+++ assign2/clock.c
@@ -13,11 +13,11 @@ static unsigned char digits[17] = { 0b00111111,   // 0
 				    0b01111111,   // 8
 				    0b01101111,   // 9
 				    0b01110111,   // A (10)
-				    0b01111100,   // B (11)
+				    0b01111100,   // b (11)
 				    0b00111001,   // C (12)
-				    0b01011110,   // D (13)
+				    0b01011110,   // d (13)
 				    0b01111001,   // E (14)
-				    0b01110001    // F (15
+				    0b01110001,   // F (15
 				    0b01000000 }; // - (16)
 
 
@@ -32,12 +32,14 @@ static unsigned int button = GPIO_PIN2;
 
 static void configure();
 static void display_single_digit( unsigned char digit_display, unsigned char digit_num );
-//static void undisplay_single_digit();
 static void display_four_digits( unsigned char digit_1, unsigned char digit_2, unsigned char digit_3, unsigned char digit_4 );
+static void start();
 
 void main(void)
 {
-    configure(); 
+    configure();
+    display_four_digits(digits[16], digits[16], digits[16], digits[16]);
+    start(); 
 //    for (int i = 0; i < 16; i = i+4) {
 //	display_single_digit( digits[i], 1 );
 //        timer_delay_ms(1000);
@@ -48,16 +50,7 @@ void main(void)
 // display ----  (digits[16])
 // click button
 // call start function:
-// set start time with timer get ticks
-// four for loops -- two outer to 9, third to 6, fourth to 9
-// while (timer get ticks - start) <  100 {display 0000 which is digits[0] for all}
-// increment digit_4 by 1 to give digits[1]
 
-
-// add some for loop logic to increment each number
-// max is 99 59
-
-    display_four_digits(digits[0], digits[1], digits[2], digits[3] );
 }
 
 
@@ -90,20 +83,39 @@ void display_single_digit( unsigned char digit_display, unsigned char digit_num 
 }
 
 
-//void undisplay_single_digit() {  // NOT NEEDED
-//    for (int i = 0; i < 8; i++) {
-//	gpio_write( segment[i] , 0);
-//    }
-//}
-//
-
 void display_four_digits( unsigned char digit_1, unsigned char  digit_2, unsigned char digit_3, unsigned char digit_4 ) {
     unsigned char to_display[4] = { digit_1, digit_2, digit_3, digit_4 };
+    for (int i = 0; i < 4; i++ ) {
+ 	display_single_digit( to_display[i], i );
+    	timer_delay_us(2500);
+    	gpio_write( digit[i] , 0 ); // turns one digit off
+    }
+}
+
+
+void start() {
+// set start time with timer get ticks
+// four for loops -- two outer to 9, third to 6, fourth to 9
+// while (timer get ticks - start) <  100 {display 0000 which is digits[0] for all}
+// increment digit_4 by 1 to give digits[1]
+
+
+// add some for loop logic to increment each number
+// max is 99 59
+    unsigned int time_start;
     while (1) {
-	for (int i = 0; i < 4; i++ ) {
-    	    display_single_digit( to_display[i], i );
-    	    timer_delay_us(2500);
-    	    gpio_write( digit[i] , 0 ); // turns one digit off
-	}
+	    for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = 0; k < 6; k++) {
+				for (int l = 0; l < 10; l++) {
+					time_start =  timer_get_ticks();
+					while ((timer_get_ticks() - time_start) < 1000*1000) {
+						display_four_digits(digits[i], digits[j], digits[k], digits[l]);
+						
+					}
+				}
+			}
+		}
+	    }
     }
 }
