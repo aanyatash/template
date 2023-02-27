deaf2071a3dcd6c9071f5eace29c8f46f5b2b02c
Aanya Tashfeen <aanyatash@gmail.com>
2023-02-26 21:41:25 -0800

    basic gl functions done

--- assign6/gl.c
+++ assign6/gl.c
@@ -16,38 +16,52 @@ void gl_swap_buffer(void)
 
 unsigned int gl_get_width(void)
 {
-    return fb_get_width;
+    return fb_get_pitch()/fb_get_depth();
 }
 
 unsigned int gl_get_height(void)
 {
-    return fb_get_width;
+    return fb_get_height();
 }
 
 color_t gl_color(unsigned char r, unsigned char g, unsigned char b)
 {
-    return | |;
+    color_t alpha = 0xff;
+    return alpha | r<<16 | g<<8 | b;
 }
 
 void gl_clear(color_t c)
 {
-    // TODO: implement this function
+    unsigned char *cptr = fb_get_draw_buffer();
+    int nbytes = gl_get_width()*gl_get_height();
+    memset(fb_get_draw_buffer, c nbytes); // fill entire framebuffer with pixels of color c
 }
 
 void gl_draw_pixel(int x, int y, color_t c)
 {
-    // TODO: implement this function
+    if (x >= gl_get_width() && y >= gl_get_height()) {
+		return;
+	}
+    unsigned int (*im)[gl_get_width()] = fb_get_draw_buffer();
+    im[y][x] = c; 
 }
 
 color_t gl_read_pixel(int x, int y)
 {
-    // TODO: implement this function
-    return 0;
+    if (x >= gl_get_width() && y >= gl_get_height()) {
+		return 0;
+	}
+    unsigned int (*im)[gl_get_width()] = fb_get_draw_buffer();
+    return im[y][x];
 }
 
 void gl_draw_rect(int x, int y, int w, int h, color_t c)
 {
-    // TODO: implement this function
+    for( int i = 0; i < w; i ++ ) {
+        for( int j = 0; j < h; j ++) {
+            draw_pixel(i + x, j + y, c);
+		}
+    }
 }
 
 void gl_draw_char(int x, int y, char ch, color_t c)
